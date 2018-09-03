a9f0d8dea99290264b9ef7e0181c96473181cda4
akshay-sri123 <akshay.srivastava@frequence.com>
2018-09-03 21:37:56 +0530

    Added operations and test cases

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -14,11 +14,47 @@ func TestCalculator(t *testing.T) {
 		a := 1
 		b := 2
 
-		c := &Calculator{var1: a, var2: b}
+		c := &Calculator{num1: a, num2: b}
 		res := c.add()
 
 		if res != 3 {
-			t.Fatal("Expected 3, got %v", res)
+			t.Fatalf("Expected 3, got %v", res)
+		}
+	})
+
+	t.Run("Should subtract the 2 numbers", func(t *testing.T) {
+		a := 6
+		b := 2
+
+		c := &Calculator{num1: a, num2: b}
+		res := c.sub()
+
+		if res != 4 {
+			t.Fatalf("Expected 4, got %v", res)
+		}
+	})
+
+	t.Run("Should multiply the 2 numbers", func(t *testing.T) {
+		a := 1
+		b := 2
+
+		c := &Calculator{num1: a, num2: b}
+		res := c.mul()
+
+		if res != 2 {
+			t.Fatalf("Expected 2, got %v", res)
+		}
+	})
+
+	t.Run("Should divide the 2 numbers", func(t *testing.T) {
+		a := 1
+		b := 2
+
+		c := &Calculator{num1: a, num2: b}
+		res := c.div()
+
+		if res != 0 {
+			t.Fatalf("Expected 0, got %v", res)
 		}
 	})
 }
--- calculator/main.go
+++ calculator/main.go
@@ -3,23 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	app = kingpin.New("library", "Library CLI")
+	app = kingpin.New("Calculator", "CLI Calculator")
 
-	aCLI = app.Command("add", "Command for adding 2 numbers.")
-	v1   = aCLI.Arg("v1", "1st value").Required().Int()
-	v2   = aCLI.Arg("v2", "2nd value").Required().Int()
+	addCommand = app.Command("add", "Adds two numbers.")
+	addArg1   = addCommand.Arg("addArg1", "1st value").Required().Int()
+	addArg2   = addCommand.Arg("addArg2", "2nd value").Required().Int()
+
+	subCommand = app.Command("sub", "Subtracts two numbers.")
+	subArg1   = subCommand.Arg("subArg1", "1st value").Required().Int()
+	subArg2   = subCommand.Arg("subArg2", "2nd value").Required().Int()
+
+	mulCommand = app.Command("mul", "Multiplies two numbers.")
+	mulArg1   = mulCommand.Arg("mulArg1", "1st value").Required().Int()
+	mulArg2   = mulCommand.Arg("mulArg2", "2nd value").Required().Int()
+
+	divCommand = app.Command("div", "Divides two numbers.")
+	divArg1   = divCommand.Arg("divArg1", "1st value").Required().Int()
+	divArg2   = divCommand.Arg("divArg2", "2nd value").Required().Int()
 )
 
 func main() {
 	var calculator *Calculator
+	// fmt.Println()
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-	case aCLI.FullCommand():
-		calculator = NewCalculator(*v1, *v2)
-		fmt.Printf("Addition is :%v\n", calculator.add())
+		case addCommand.FullCommand():
+			calculator = NewCalculator(*addArg1, *addArg2)
+			fmt.Printf("Result is: %v\n", calculator.add())
+
+		case subCommand.FullCommand():
+			calculator = NewCalculator(*subArg1, *subArg2)
+			fmt.Printf("Result is: %v\n", calculator.sub())
+
+		case mulCommand.FullCommand():
+			calculator = NewCalculator(*mulArg1, *mulArg2)
+			fmt.Printf("Result is: %v\n", calculator.mul())
+
+		case divCommand.FullCommand():
+			calculator = NewCalculator(*divArg1, *divArg2)
+			fmt.Printf("Result is: %v\n", calculator.div())
 	}
 }
